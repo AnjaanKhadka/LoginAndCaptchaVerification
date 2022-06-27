613d2fd3c3cf90b52c58cb374527784a003d7f93
AnjaanKhadka <njn6171@gmail.com>
2022-06-27 19:03:14 +0545

    Updated for login and signup

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ var c = document.getElementById("CaptchaCanvas");
 var ctx = c.getContext("2d");
 var cpt;
 
+var isLen = false;
+var isUcase = false;
+var isLcase = false;
+var isNum = false;
+var isSchar = false;
+
 function StartUp(){
 
     //Initiating with Null feedback text
@@ -20,73 +26,148 @@ function wait(ms){
    }
  }
 
-//Function to display feedback text
+// Function to display feedback text
 function WriteFeedback(Message){
     var feedback = document.getElementById("FeedbackText");
     feedback.textContent = Message;
 }
 
+function UpdateRadio_tick(id){
+    document.getElementById(id).classList.remove("CrossList");
+    document.getElementById(id).classList.add("TickList");
+}
+
+function UpdateRadio_cross(id){
+    document.getElementById(id).classList.remove("TickList");
+    document.getElementById(id).classList.add("CrossList");
+}
+
+// function to update feedback list
+function UpdateFeedback(){
+    if (isLen) UpdateRadio_tick("LengthVerify");   
+    else UpdateRadio_cross("LengthVerify");
+        
+    if (isLcase) UpdateRadio_tick("LowerVerify");   
+    else UpdateRadio_cross("LowerVerify");
+
+    if (isUcase) UpdateRadio_tick("UpperVerify");   
+    else UpdateRadio_cross("UpperVerify");
+
+    if (isNum) UpdateRadio_tick("NumVerify");   
+    else UpdateRadio_cross("NumVerify");
+
+    if (isSchar) UpdateRadio_tick("CharVerify");   
+    else UpdateRadio_cross("CharVerify");
+
+}
+
+
 function CheckCharacters(pass){
-    var isLcase = 0;
-    var isUcase = 0;
-    var isNum = 0;
-    var isSchar = 0;
+    var isLcase2 = false;
+    var isUcase2 = false;
+    var isNum2 = false;
+    var isSchar2 = false;
     var i = pass.length;
+    var len = i;
     while (i--) {
         ch = pass.charCodeAt(i);
         if(ch>=32 && ch<=126){
             if(ch>=48 && ch<=57){
-                isNum = 1;
+                isNum2 = true;
             }
             else if(ch>=65 && ch<=90){
-                isUcase = 1;
+                isUcase2 = true;
             }
             else if(ch>=97 && ch<=122){
-                isLcase = 1;
+                isLcase2 = true;
             }
             else{            
-                isSchar = 1;
+                isSchar2 = true;
             }
         }         
     }
-    if(!isLcase){
-        WriteFeedback("Your password needs to include Lowercase Letters");
-        return false;
+    // if(!isLcase){
+    //     WriteFeedback("Your password needs to include Lowercase Letters");
+    //     return false;
+    // }
+    // if(!isUcase){
+    //     WriteFeedback("Your password needs to include Uppercase Letters");
+    //     return false;
+    // }
+    // if(!isNum){
+    //     WriteFeedback("Your password needs to atleast one number");
+    //     return false;
+    // }
+    // if(!isSchar){
+    //     WriteFeedback("Your password needs to include special characters");
+    //     return false;
+    // }
+    // return true
+
+    var ret = true;
+    if(len<8){
+        isLen = false;
+        ret = false;
     }
-    if(!isUcase){
-        WriteFeedback("Your password needs to include Uppercase Letters");
-        return false;
+    else{
+        isLen = true;
+    }   
+    if(!isLcase2){
+        isLcase = false;
+        ret = false;
     }
-    if(!isNum){
-        WriteFeedback("Your password needs to atleast one number");
-        return false;
+    else{
+        isLcase = true;
     }
-    if(!isSchar){
-        WriteFeedback("Your password needs to include special characters");
-        return false;
+    if(!isUcase2){
+        isUcase = false;
+        ret = false;
     }
-    return true
+    else{
+        isUcase = true;
+    }
+    if(!isNum2){
+        isNum = false;
+        ret = false;
+    }
+    else{
+        isNum = true;
+    }
+    if(!isSchar2){
+        isSchar = false;
+        ret = false;
+    }
+    else{
+        isSchar = true;
+    }
+    UpdateFeedback();
+    return ret
+
 }
 
 //Algorithmic way of checking weak password.
 function CheckPassword(){
     psw = document.getElementById("PasswordID").value;
     psw2 = document.getElementById("PasswordID2").value;
+    var ret = true;
     if (psw!=psw2){
         WriteFeedback("Passwords do not match !!");
-        return false;
+        ret = false;
     }
-    if (psw.length < 8){
-        WriteFeedback("Password should be atleast 8 characters long");
-        return false;
+    else{
+        WriteFeedback(" ")
     }
+    // if (psw.length < 8){
+    //     WriteFeedback("Password should be atleast 8 characters long");
+    //     return false;
+    // }
     if(!CheckCharacters(psw)){
-        return false;
+        ret = false;
     }
 
-    WriteFeedback("This is Working");
+    // WriteFeedback("This is Working");
     // console.log(psw);
-    return true;
+    return ret;
 }
 
 function refillCanvas(){
@@ -168,6 +249,35 @@ function SubmitBtnClick(){
         return false;
     }
     wait(3000)
-    window.location.href = 'main.html';
-    
+    window.location.href = 'main.html'; 
 }
+
+function LoginPageLoad(){
+    window.location.href = "Login.html";
+}
+
+function SignUpPageLoad(){
+    window.location.href = "Signup.html";
+}
+
+function LoginVerification(){
+    // Write your code for Login Verification 
+    EmailID = document.getElementById("EmailID").value;
+    password = document.getElementById("PasswordID").value;
+
+    Verified = true
+    //Your verification code here
+
+
+    
+    if (Verified){
+        window.location.href = "main.html";
+    }
+    else{
+        WriteFeedback("Your Email or Password is Incorrect");
+    }
+}
+
+setInterval(function(){ 
+    CheckPassword(); 
+}, 2000);
